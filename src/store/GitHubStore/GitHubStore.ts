9b5480dcb50251b9527415839305ec8ae123a8c2
prorokky <vlad6005@gmail.com>
2021-08-25 11:58:12 +0300

    change GitHubStore

--- src/store/GitHubStore/GitHubStore.ts
+++ src/store/GitHubStore/GitHubStore.ts
@@ -1,13 +1,22 @@
 import ApiStore from '../../shared/store/ApiStore';
-import {IGitHubStore} from "./types";
+import {GetOrganizationReposListParams, IGitHubStore, RepoItem} from "./types";
+import {ApiResponse, HTTPMethod} from "../../shared/store/ApiStore/types";
+
+const BASE_URL = 'https://api.github.com'
 
 export default class GitHubStore implements IGitHubStore {
-    private readonly apiStore = new ApiStore(); // TODO: не забудьте передать baseUrl в конструктор
+    private readonly apiStore = new ApiStore(BASE_URL); // TODO: не забудьте передать baseUrl в конструктор
 
     // TODO: реализовать интерфейс IGitHubStore
 
-    async getOrganizationReposList(params: GetOrganizationReposListParams): Promise<ApiResp<RepoItem[]>> {
+    async getOrganizationReposList(params: GetOrganizationReposListParams): Promise<ApiResponse<RepoItem[], any>> {
         // TODO: Здесь сделайте вызов из this.apiStore и верните результат
         // Документация github: https://docs.github.com/en/rest/reference/repos#list-organization-repositories
+        return await this.apiStore.request({
+            method: HTTPMethod.GET,
+            data: {},
+            headers: {},
+            endpoint: `/orgs/${params.organizationName}/repos`
+        })
     }
 }
--- src/store/GitHubStore/types.ts
+++ src/store/GitHubStore/types.ts
@@ -5,6 +5,14 @@
  * Или не меняйте, если делаете запрос за списком репоизториев для организации)
  * Выберите любой запрос из публичного API GitHub.
  */
+import {ApiResponse} from "../../shared/store/ApiStore/types";
+
+export type GetOrganizationReposListParams = {
+    organizationName: string
+}
+
+export type RepoItem = {}
+
 export interface IGitHubStore {
-    getOrganizationReposList(params: GetOrganizationReposListParams): Promise<ApiResp<RepoItem[]>>;
+    getOrganizationReposList(params: GetOrganizationReposListParams): Promise<ApiResponse<RepoItem[], any>>;
 }
