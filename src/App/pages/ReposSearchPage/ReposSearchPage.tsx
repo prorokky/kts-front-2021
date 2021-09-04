06953447d4b99b34fc655a68b6984708512489ee
prorokky <vlad6005@gmail.com>
2021-09-04 14:43:40 +0300

    add react fragment

--- src/App/pages/ReposSearchPage/ReposSearchPage.tsx
+++ src/App/pages/ReposSearchPage/ReposSearchPage.tsx
@@ -1,63 +1,63 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-import Avatar from '@components/Avatar'
-import Button from '@components/Button'
-import Input from '@components/Input'
-import RepoTile from '@components/RepoTile'
-import SearchIcon from '@components/SearchIcon'
-import './ReposSearchPage.css'
-import { getOrganizationReposListFetch } from '@root/root'
-import { RepoItem } from 'src/store/GitHubStore/types'
+import Avatar from "@components/Avatar";
+import Button from "@components/Button";
+import Input from "@components/Input";
+import RepoTile from "@components/RepoTile";
+import SearchIcon from "@components/SearchIcon";
+import "./ReposSearchPage.css";
+import { getOrganizationReposListFetch } from "@root/root";
+import { RepoItem } from "src/store/GitHubStore/types";
 
 const ReposSearchPage = () => {
-    const [value, setValue] = useState('')
-    const [isLoading, setIsLoading] = useState(false)
-    const [repos, setRepos] = useState<RepoItem[]>([])
+  const [value, setValue] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [repos, setRepos] = useState<RepoItem[]>([]);
 
-    const handlerInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setValue(event.target.value)
-    }
+  const handlerInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
 
-    const handlerButton = (event: React.MouseEvent) => {
-        event.preventDefault()
-        setIsLoading(true)
-        getOrganizationReposListFetch(value).then(value => {
-            if (value) {
-                setRepos(value)
-            } else {
-                alert('Что-то пошло не так!')
-            }
-        })
-        setValue('')
-        setIsLoading(false)
-    }   
-   
-    return (
-        <div>
-            <form className='search-line'>
-                <Input value={value} onChange={handlerInput}/>
-                <Button disabled={isLoading} onClick={handlerButton}>
-                    <SearchIcon/>
-                </Button>
-            </form>
-            {repos.map(el => {
-                return <div className='card-block'>
-                            <div className='card'>
-                                <React.Fragment key={el.owner.id}>
-                                    <Avatar 
-                                        src={el.owner.avatar_url}
-                                        alt='repo_img'
-                                        letter={el.name.substring(0,1)}
-                                    />
-                                    <RepoTile
-                                        item={el}
-                                    />
-                                </React.Fragment>
-                            </div>
-                        </div>
-            })}
-        </div>
-    )
-}
+  const handlerButton = (event: React.MouseEvent) => {
+    event.preventDefault();
+    setIsLoading(true);
+    getOrganizationReposListFetch(value).then((value) => {
+      if (value) {
+        setRepos(value);
+      } else {
+        alert("Что-то пошло не так!");
+      }
+    });
+    setValue("");
+    setIsLoading(false);
+  };
 
-export default ReposSearchPage
+  return (
+    <div>
+      <form className="search-line">
+        <Input value={value} onChange={handlerInput} />
+        <Button disabled={isLoading} onClick={handlerButton}>
+          <SearchIcon />
+        </Button>
+      </form>
+      {repos.map((el) => {
+        return (
+          <div className="card-block">
+            <React.Fragment key={el.id}>
+              <div className="card">
+                <Avatar
+                  src={el.owner.avatar_url}
+                  alt="repo_img"
+                  letter={el.name.substring(0, 1).toUpperCase()}
+                />
+                <RepoTile item={el} />
+              </div>
+            </React.Fragment>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default ReposSearchPage;
--- src/components/RepoTile/RepoTile.tsx
+++ src/components/RepoTile/RepoTile.tsx
@@ -1,37 +1,35 @@
-import React from 'react';
+import React from "react";
 
-import StarIcon from '@components/StarIcon';
-import { RepoItem } from 'src/store/GitHubStore/types';
+import StarIcon from "@components/StarIcon";
+import { RepoItem } from "src/store/GitHubStore/types";
 
-import './RepoTile.css'
+import "./RepoTile.css";
 
 type RepoTileProps = {
-    item: RepoItem,
-    onClick?: (event: React.MouseEvent) => void
-}
+  item: RepoItem;
+  onClick?: (event: React.MouseEvent) => void;
+};
 
-const RepoTile: React.FC<RepoTileProps> = ({ item, onClick}) => {
-    return (
-        <div className='card__content'>
-                <div className='card__repo-info'>
-                    <span className='card__repo-name'>
-                        {item.name ? item.name : ''}
-                    </span>
-                    <a href='https://kts.studio/' className='card__company-name'>
-                        {item.owner.login}
-                    </a>
-                </div>
-                <div className='card__other-info'>
-                    <span className='card__stars'>
-                        {<StarIcon/>}
-                        {item.stargazers_count}
-                    </span>
-                    <span className='card__update-time'>
-                        {item.updated_at.substring(0,10)}
-                    </span>
-                </div>
-            </div>
-    )
-}
+const RepoTile: React.FC<RepoTileProps> = ({ item, onClick }) => {
+  return (
+    <div className="card__content">
+      <div className="card__repo-info">
+        <span className="card__repo-name">{item.name ? item.name : ""}</span>
+        <a href="https://kts.studio/" className="card__company-name">
+          {item.owner.login}
+        </a>
+      </div>
+      <div className="card__other-info">
+        <span className="card__stars">
+          {<StarIcon />}
+          {item.stargazers_count}
+        </span>
+        <span className="card__update-time">
+          {item.updated_at.substring(0, 10)}
+        </span>
+      </div>
+    </div>
+  );
+};
 
-export default RepoTile
+export default RepoTile;
