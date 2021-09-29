f57c368cde78cac8d11576bbe7cba13f6fcf7354
prorokky <vlad6005@gmail.com>
2021-09-29 23:22:43 +0300

    add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,33 @@
 const path = require('path')
-
+const HTMLWebpackPlugin = require('html-webpack-plugin')
 const buildPath = path.resolve(__dirname, 'dist')
+const srcPath = path.resolve(__dirname, 'src')
 
 module.exports = {
-    entry: path.resolve(__dirname, './index.tsx'),
+    entry: path.resolve(__dirname, './src/index.tsx'),
     output: {
         path: buildPath,
         filename: "bundle.js"
     },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: path.join(srcPath, 'index.html')
+        })
+    ],
     module: {
         rules: [
+            {
+              test: /\.css/,
+              use: ['style-loader', 'css-loader']
+            },
             {
                 test: /\.([tj])sx?$/,
                 use: 'babel-loader'
             }
         ]
+    },
+    devServer: {
+        host: 'localhost',
+        port: 3000
     }
 }
