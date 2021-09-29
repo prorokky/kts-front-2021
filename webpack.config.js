5e70e9554f19754bf710f86eab2e19023eac9ec5
prorokky <vlad6005@gmail.com>
2021-09-29 23:52:48 +0300

    add refresh plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+
 const buildPath = path.resolve(__dirname, 'dist')
 const srcPath = path.resolve(__dirname, 'src')
 
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
     entry: path.resolve(__dirname, './src/index.tsx'),
+    target: !isProd ? 'web' : 'browserslist',
     output: {
         path: buildPath,
         filename: "bundle.js"
@@ -12,8 +17,9 @@ module.exports = {
     plugins: [
         new HTMLWebpackPlugin({
             template: path.join(srcPath, 'index.html')
-        })
-    ],
+        }),
+        isProd && new ReactRefreshWebpackPlugin()
+    ].filter(Boolean),
     module: {
         rules: [
             {
@@ -28,6 +34,8 @@ module.exports = {
     },
     devServer: {
         host: 'localhost',
-        port: 3000
+        port: 3000,
+        hot: true,
+        inline: true
     }
 }
