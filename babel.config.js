43c909b190e545c1826d0e703f0e8762020473ea
prorokky <vlad6005@gmail.com>
2021-09-30 11:08:22 +0300

    finish webpack settings

--- babel.config.js
+++ babel.config.js
@@ -3,10 +3,13 @@ module.exports = api => {
 
     const presets = [
         '@babel/preset-env',
-        '@babel/preset-react'
+        '@babel/preset-react',
+        '@babel/preset-typescript',
+        'mobx'
     ]
 
     const plugins = [
+        '@babel/plugin-proposal-optional-chaining',
         process.env.NODE_ENV === 'development' && 'react-refresh/babel'
     ].filter(Boolean)
 
--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'regenerator-runtime'
+
 ReactDOM.render(
   <React.StrictMode>
     <App /> 
