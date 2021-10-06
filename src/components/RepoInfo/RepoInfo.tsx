43a58759f5197dbbb6a349fa9f1d2b3dcb42bc72
prorokky <vlad6005@gmail.com>
2021-10-06 18:01:44 +0300

    add repo info

---
+++ src/components/RepoInfo/RepoInfo.tsx
@@ -0,0 +1,23 @@
+import React, {useEffect} from 'react'
+
+import RepoInfoStyle from './RepoInfo.module.scss'
+import {RepoItemModel} from "@store/models/gitHubRepos";
+
+type RepoInfoProps = {
+    org: RepoItemModel
+}
+
+const RepoInfo: React.FC<RepoInfoProps> = ({ org }) => {
+    return(
+        <div>
+            {org.description && <p>Описание: {org.description}</p>}
+            {org.description && <p>Главная ветка: {org.default_branch}</p>}
+            {org.language && <p>Язык: {org.language}</p>}
+            {org.homepage && <p>Домашняя страница: {org.homepage}</p>}
+            {org.watchers_count != 0 ? <p>Количество наблюдателей: {org.watchers_count}</p> : ""}
+            {org.forks_count != 0 ? <p>Количество ответвлений (forks): {org.forks_count}</p> : ""}
+        </div>
+    )
+}
+
+export default RepoInfo
---
+++ src/components/RepoInfo/index.ts
@@ -0,0 +1,3 @@
+export { default } from './RepoInfo'
+
+export * from './RepoInfo'
